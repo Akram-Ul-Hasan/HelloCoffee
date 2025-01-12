1d7fffe3a56ac0e8ab34860f3362a66a2553d71b
Akram-Ul-Hasan <akramulhasan.dev@gmail.com>
2025-01-12 12:52:14 +0600

    Initial Commit

---
+++ HelloCoffee/HelloCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCoffeeApp.swift
+//  HelloCoffee
+//
+//  Created by Akram Ul Hasan on 12/1/25.
+//
+
+import SwiftUI
+
+@main
+struct HelloCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
